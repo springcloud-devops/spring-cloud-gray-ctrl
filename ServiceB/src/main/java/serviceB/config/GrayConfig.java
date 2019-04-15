ab14b139bec45cad07d3627ec8891a69a8c0b93a
郭志华 <guozh@cheche365.com>
2019-04-15 16:42:45 +0800

    抽取为cloud start 组件，减少代码耦合

--- ServiceB/src/main/java/serviceB/config/GrayConfig.java
+++ ServiceB/src/main/java/serviceB/config/GrayConfig.java
@@ -4,6 +4,6 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan(basePackages = {"org.ext.sc.hystrix","api"})
+@ComponentScan(basePackages = {"api"})
 public class GrayConfig {
 }
--- spring-cloud-gray-ctrl-starter/src/main/java/org/ext/sc/hystrix/config/FeignSupportConfig.java
+++ spring-cloud-gray-ctrl-starter/src/main/java/org/ext/sc/hystrix/config/FeignSupportConfig.java
@@ -6,6 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 
